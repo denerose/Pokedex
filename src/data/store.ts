c30f563927f3c2376864418d2f3075938ee94465
Saphron H <109723086+denerose@users.noreply.github.com>
2024-11-26 12:47:00 +1100

    Create pokemon store

--- src/data/store.ts
+++ src/data/store.ts
@@ -1,5 +1,20 @@
 import { defineStore } from 'pinia'
+import { Pokemon } from './types';
+import { ref } from 'vue';
 
 export const useCounterStore = defineStore('pokemon', () => {
+
+    const pokemon = ref<Pokemon | null>(null);
+    const pokeList = ref<Pokemon[]>([]);
+
+    function setPokemon(value: Pokemon) {
+        pokemon.value = value;
+    }
+
+    return {
+        pokemon,
+        pokeList,
+        setPokemon
+    };
     
-}
+});
