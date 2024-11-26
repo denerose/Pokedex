b0100ac76e2173596b4121c7fdb57e87f0dbdd8c
Saphron H <109723086+denerose@users.noreply.github.com>
2024-11-26 15:10:04 +1100

    Store working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import { usePokeStore } from './data/store';
+
+  const store = usePokeStore();
+
+  if (store.$state.pokeList.length === 0) {
+    store.updatePokeList();
+  }
+
+  const pokeList = store.pokeList;
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
+  <div class="card">
+    <h2>Pokemon List</h2>
+    <ul>
+      <li v-for="poke in pokeList" :key="poke.name">{{ poke.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
